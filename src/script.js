329f9a35e862d08c9b74ad2f2dead6e877db2693
annandeborah <annandeborah@gmail.com>
2026-04-01 12:54:15 +0000

    add all

---
+++ src/script.js
@@ -0,0 +1,27 @@
+alert("Hello User!");
+let x = 2;
+
+
+
+let age; 
+age = 8;
+// let name = "Abigail";
+// // alert("Hello"name);
+// alert(age);
+
+// age = 10; reassigning the value of age from 8 to 18
+// alert(age);
+
+// const age1 = 15;
+// alert(age1);
+
+// age1 = 21;
+// alert(age1);
+
+// prompt("What is your name?");
+let userName = prompt("What is your name?");
+alert("Hello " + userName);
+
+alert(`Hello ${userName}! Welcome to JavaScript!`);
+let sentence = "I" + " am " + "here " + "to learn JavaScript.";
+alert(sentence);
